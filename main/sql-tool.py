56fa6c8730be064d9f372c28ffd31d4fea05426c
panda <386994725@qq.com>
2017-11-05 11:32:13 +0800

    some modification

--- main/sql-tool.py
+++ main/sql-tool.py
@@ -1,4 +1,30 @@
-import argparser
+import argparse
+
+def provide_help():
+    print "provide help"
+
+def command_tool_mode():
+    print "command tool mode"
 
 def main():
-    pass
+    # create parser
+    descStr = """
+    This program provides some helpful functions for ZW group in CM-GZ
+    """
+    parser = argparse.ArgumentParser(description=descStr)
+    
+    # add a mutually exclusive group of arguments
+    group = parser.add_mutually_exclusive_group()
+    
+    # add expected arguments
+    group.add_argument('--help',action="provide_help", required=False)
+    group.add_argument('--s', action="command_tool_mode", required=False)
+    
+    # parse args
+    args = parser.parse_args()
+    
+
+
+
+if __name__ == '__main__':
+    main()
