d1c13a604008bf49a01f6220974f58b9c1aeb533
panda <386994725@qq.com>
2017-11-05 14:00:14 +0800

    little change

--- main/sql-tool.py
+++ main/sql-tool.py
@@ -1,8 +1,17 @@
 import argparse
+import readline
 
 def command_tool_mode():
     while 1:
         input = raw_input("command >> ")
+        if input in ['exit','exit()']:
+            break
+
+def volatility(files):
+    if len(files) < 2: print "Required at least two files"
+
+def initialize():
+    print "init"
 
 def main():
     # create parser
@@ -15,10 +24,15 @@ def main():
     group = parser.add_mutually_exclusive_group()
     
     # add expected arguments
-    group.add_argument('-s', action="store_true",
-                       help="shell mode",required=False)
-    # group.add_argument('--s', action="command_tool_mode", required=False)
-    
+    group.add_argument('-s', action='store_true', 
+                       help='shell mode',required=False)
+    group.add_argument('-init',action='store_true',
+                       help='create sqlite file and configuration file',
+                       required=False)
+    group.add_argument('-vol', nargs='*',
+                       help='volatility of files with same structure. tips: -vol file1 ,file2, [file3, ...]',
+                       required=False)
+
     # parse args
     args = parser.parse_args()
     
@@ -27,11 +41,13 @@ def main():
             command_tool_mode()
         except KeyboardInterrupt:
             print("Goodbye~")
+    elif args.vol:
+        volatility(args.vol)
+    elif args.init:
+        initialize()
     else:
         parser.print_help()
     
-
-
 
 if __name__ == '__main__':
     main()
